0576cdc160e67fb4b6e0541f6f30f1b6ab05a772
2439905184 <suchenglong018@126.com>
2020-05-22 09:52:09 +0800

    使用了全新的方法 解决了form表单提交有时得不到返回结果的问题 并且支持了单选 下次会加入一键搜索吧 23333  window.open(url) 23333333333333333333333333333

--- core.js
+++ core.js
@@ -14,6 +14,7 @@ var  chebox_node2=document.getElementById("check2")
    // role="search" method="get"
     var action_4399="http://so2.4399.com/search/search.php?k="
     var action_need_add_4399="?k="
+    //steam
    var  action_steam="https://store.steampowered.com/search/?term="
     var action_itch_io="https://itch.io/search?q="
     //国内独立游戏网站
@@ -33,21 +34,68 @@ if(chebox_node1.checked==true)
      console.log(action_node.action)
 }
 }   
-function tap_check()
+//通用
+function tap_check(id,checked)
 {
+    //console.log(id)
+    var current_checkbox=document.getElementById(id)
+console.log(id+":"+current_checkbox.checked)
+if(current_checkbox.checked==true &&id=="check_steam")
+{
+    action=action_steam
+    console.log("steam")
+     action_node.action=action_steam+to_search_string
+     console.log(action_node.action)
+    
+}
+if(current_checkbox.checked==true &&id=="check2")
+{
+    action=action_taptap
+    console.log("steam")
+     action_node.action=action_taptap+to_search_string
+     console.log(action_node.action)
+    
+}
+if(current_checkbox.checked==true &&id=="indian")
+{
+    action=action_india
+    console.log("indian独立游戏")
+     action_node.action=action_india+to_search_string
+     console.log(action_node.action)
+    
+}
+if(current_checkbox.checked==true &&id=="itch_io")
+{
+    action=action_itch_io
+    console.log("itch.io")
+     action_node.action=action_itch_io+to_search_string
+     console.log(action_node.action)
+     
+    
+}
+if(current_checkbox.checked==true &&id=="block")
+{
+    action=action_block
+    console.log("block")
+     action_node.action=action_block+to_search_string
+     console.log(action_node.action)
+    
+}
+//console.log(chebox_node2.checked)
+//console.log(id+":"+checked)
 
-console.log(chebox_node2.checked)
-if(chebox_node2.checked==true)
+/*if(chebox_node2.checked==true)
 {
     action=action_taptap
     console.log("4399")
      action_node.action=action_taptap+to_search_string
      console.log(action_node.action)
-}
-else
+     console.log(id)
+}*/
+/*else
 {
     action_node.action="404.html"
-}
+}*/
 }
    function usr_input()
     {
@@ -66,4 +114,5 @@ else
    // alert(action_node.action)
     console.log("search!")
           console.log(action_node.action)
+          window.open(action_node.action,"_blank")
     }
